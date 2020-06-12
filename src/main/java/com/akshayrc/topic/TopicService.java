8c2dfceea8cc8649a15fbc155b7cc2bbcd63db6c
akshaychaudhari <akshaychaudhari12345@gmail.com>
2020-06-13 02:13:10 +0530

    GET and POST implemented for topic entity(resourse)

--- src/main/java/com/akshayrc/topic/TopicService.java
+++ src/main/java/com/akshayrc/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.akshayrc.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	List<Topic> topics = Arrays.asList(
+	List<Topic> topics =   new ArrayList<> (Arrays.asList(
 			new Topic("001","Spring","Framework"),
 			new Topic("002","Java","Programming Language"),
 			new Topic("003","Python","Scripting Language")
-			);
+			));
 
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -20,6 +21,14 @@ public class TopicService {
 
 	public void setTopics(List<Topic> topics) {
 		this.topics = topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 	
 }
--- src/main/java/com/akshayrc/topic/TopicsController.java
+++ src/main/java/com/akshayrc/topic/TopicsController.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +16,18 @@ public class TopicsController {
 	@Autowired
 	private TopicService topicService;
 	
-	@RequestMapping("/GetTopics")
+	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 		return  topicService.getAllTopics();
 	}
 
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
 }
